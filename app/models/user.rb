6de8749eb87f83ab06a27fffc882e7ceb1d27f83
yoshi23s <classa1990@gmail.com>
2022-07-12 03:44:31 +0900

    ユーザー新規登録時バリデーション/エラーメッセージ設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  validates :nickname,        presence: true
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Input full-width characters' }
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Input full-width characters' }
+  validates :last_name_kana,  presence: true, format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: "Input full-width katakana characters"}
+  validates :first_name_kana, presence: true, format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/,message: "Input full-width katakana characters"}
+  validates :birth_date,      presence: true
+  validates :password, format: { with: /[a-z\d]{6,}/i, message: "Include both letters and numbers" }, confirmation: true
+
   has_many :items
 end
