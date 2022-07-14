dd856c3d967403269224aba25833c91c5aba485c
yoshi23s <classa1990@gmail.com>
2022-07-15 01:06:38 +0900

    itemのルーティング作成とerb記入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,17 @@ class ItemsController < ApplicationController
   end
   
   def new
+    @item = Item.new
   end
   
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_info, :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :item_scheduled_delivery_id, :price, :user)
+  end
+
+                        
 end
