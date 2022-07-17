8fd9d4f407fd2d2b2bb4872d01846ab5688970db
yoshi23s <classa1990@gmail.com>
2022-07-17 16:53:24 +0900

    商品編集機能レビュー後1回目(itemコントローラーbeforeActionでリファクタリング)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:edit, :show, :update]
+  
 
 
   def index
@@ -21,15 +23,12 @@ class ItemsController < ApplicationController
   end
   
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -49,6 +48,9 @@ class ItemsController < ApplicationController
      end
   end
   
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
           
 end
