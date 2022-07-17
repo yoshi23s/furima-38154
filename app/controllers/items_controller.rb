f145f5f93c12419a86cac36a799c5979bc4cdde4
yoshi23s <classa1990@gmail.com>
2022-07-17 16:41:47 +0900

    編集機能の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
+
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -22,13 +24,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
 
   private
   def item_params
     params.require(:item).permit(:image, :item_name, :item_info, :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :item_scheduled_delivery_id, :price, :user).merge(user_id: current_user.id)
   end
 
-
+  def move_to_index
+    @item = Item.find(params[:id])
+     unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+     end
+  end
   
 
           
