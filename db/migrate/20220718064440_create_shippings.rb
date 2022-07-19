2551b96044388f65f9e21a742bcaeebccdcd9116
yoshi23s <classa1990@gmail.com>
2022-07-19 12:29:32 +0900

    orders/indexの作成とコントローラーなど修正

--- db/migrate/20220718064440_create_shippings.rb
+++ db/migrate/20220718064440_create_shippings.rb
@@ -1,13 +1,14 @@
 class CreateShippings < ActiveRecord::Migration[6.0]
   def change
     create_table :shippings do |t|
-      t.string     :postal_code,   null false, default: ""
-      t.string     :prefecture_id, null false, default: ""
-      t.string     :city,          null false, default: ""
-      t.string     :address,       null false, default: ""
-      t.string     :building,                  default: ""
-      t.string     :phone_number,  null false, default: ""
-      t.references :order,         null false, default: "", foreign_key: true
+      t.string     :postal_code,   null: false, default: ""
+      t.string     :prefecture_id, null: false, default: ""
+      t.string     :city,          null: false, default: ""
+      t.string     :address,       null: false, default: ""
+      t.string     :building,                   default: ""
+      t.string     :phone_number,  null: false, default: ""
+      t.references :order,         null: false, foreign_key: true
+
       t.timestamps
     end
   end
---
+++ spec/requests/shippings_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Shippings", type: :request do
+
+end
